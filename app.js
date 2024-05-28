4f7f400d28c822340806827c0b4bcb451de86b04
MosheD13 <127778461+MosheD13@users.noreply.github.com>
2024-05-28 13:23:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-// 1Import required modules
 const express = require('express');
-
-// Create an Express application
 const app = express();
+const port = 3000;
 
-// Define a route handler for the root URL
 app.get('/', (req, res) => {
   res.send('AM ISRAEL HAI!');
 });
 
-// Start the server on port 3000
-const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
 
+module.exports = app;
